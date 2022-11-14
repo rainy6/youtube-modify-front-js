adb14699a11c8028440bc4d115ed48d8cca20ad9
rainy6 <ryo167prog@gmail.com>
2022-11-14 23:25:50 +0900

    ESmodule 移行要

---
+++ layouts/default.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <TheHeader />
+
+    <div class="columns">
+      <TheSideBar />
+
+      <div class="column content">
+        <nuxt/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import TheHeader from "~/components/TheHeader";
+  import TheSideBar from "~/components/TheSideBar";
+  export default {
+    components: {
+      TheHeader,
+      TheSideBar
+    }
+  }
+</script>
+
+<style scoped>
+  #app {
+    min-height: 100vh;
+    background: #FAFAFA;
+  }
+  .columns {
+    min-height: 100vh;
+  }
+</style>
